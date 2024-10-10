e87d83b2362c113b0aa5c17c941128f8ec5b5be1
Silvan Schmidt <mail@silvanschmidt.ch>
2024-10-10 21:47:48 +0200

    feat: create thoughts

---
+++ src/cli.rs
@@ -0,0 +1,8 @@
+use clap::Command;
+
+pub fn get_cli() -> Command {
+    Command::new("thought")
+        .author("Silvan Schmidt <mail@silvanschmidt.ch>")
+        .about("Save your thoughts for later.")
+        .subcommand(Command::new("new"))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+mod cli;
+pub(crate) use cli::get_cli;
+
+mod editor_wrapper;
+pub(crate) use editor_wrapper::EditorWrapper;
+
 mod error;
 pub(crate) use error::Error;
 
@@ -8,5 +14,20 @@ mod words;
 pub(crate) use words::WORDS;
 
 pub fn run() -> Result<(), Error> {
+    let base_dirs = xdg::BaseDirectories::with_prefix("thought").map_err(|_| todo!())?;
+
+    let matches = get_cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("new", _)) | None => {
+            let thought_id = ThoughtId::new();
+            let thought_path = base_dirs
+                .place_data_file(format!("{}.md", thought_id))
+                .map_err(|_| todo!())?;
+            EditorWrapper::new().edit(thought_path)?;
+        }
+        _ => unreachable!(),
+    }
+
     Ok(())
 }
