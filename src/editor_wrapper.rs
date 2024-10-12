ff42a962d2f2ad8e4537cd66c9d359cfd403560e
Silvan Schmidt <mail@silvanschmidt.ch>
2024-10-12 17:53:10 +0200

    fix: remove not fully implemented editor_override

--- src/editor_wrapper.rs
+++ src/editor_wrapper.rs
@@ -2,30 +2,17 @@ use std::path::Path;
 
 use crate::Error;
 
-pub struct EditorWrapper {
-    editor_override: Option<String>,
-}
+pub struct EditorWrapper {}
 
 impl EditorWrapper {
     pub fn new() -> Self {
-        Self {
-            editor_override: None,
-        }
-    }
-
-    pub fn new_with_override(editor_override: impl Into<String>) -> Self {
-        Self {
-            editor_override: Some(editor_override.into()),
-        }
+        Self {}
     }
 
     pub fn edit<P: AsRef<Path>>(&self, file_path: P) -> Result<(), Error> {
         let file_path = file_path.as_ref();
 
-        let editor_command = self
-            .editor_override
-            .clone()
-            .unwrap_or(std::env::var("EDITOR").unwrap_or("xdg-open".to_string()));
+        let editor_command = std::env::var("EDITOR").unwrap_or("xdg-open".to_string());
 
         let _ = std::process::Command::new(editor_command)
             .arg(file_path)
