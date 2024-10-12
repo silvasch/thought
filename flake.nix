7baa627fd47e440bfc083befa549ab48b0dc31af
Silvan Schmidt <mail@silvanschmidt.ch>
2024-10-12 17:55:24 +0200

    fix: bump version in flake.nix too

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         in {
           default = toolchain.buildRustPackage {
             pname = "thought";
-            version = "0.1.0";
+            version = "1.0.1";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
           };
